2abe25864e3bb2b51ff6f799a5bdcfb5b765039c
Axect <edeftg@gmail.com>
2018-04-16 20:31:36 +0900

    Image Charge Dipole - Ver.1

--- dipole.jl
+++ dipole.jl
@@ -1,32 +1,55 @@
 using PyPlot
 
+struct Params
+  q :: Float64 # charge
+  r :: Float64 # radius of circle
+end
+
 mutable struct Position
   x :: Float64
   y :: Float64
 end
 
-function Potential(q::Float64, P1::Position, P2::Position)
+function native_dipole(q::Float64, P1::Position, P2::Position)
   (x1, y1) = (P1.x, P1.y);
   (x2, y2) = (P2.x, P2.y);
   return (x,y) -> q * (1/sqrt((x-x1)^2 + (y-y1)^2) - 1/sqrt((x-x2)^2 + (y-y2)^2))
 end
 
+function image_dipole(p::Params, P1::Position, P2::Position)
+  # x-axis dipole
+  (q, r) = (p.q, p.r);
+  (x1, y1) = (P1.x, P1.y);
+  (x2, y2) = (P2.x, P2.y);
+  l1 = sqrt(x1^2 + y1^2); # distance of q1 (Center of Circle = (0,0))
+  l2 = sqrt(x2^2 + y2^2); # distance of q2
+  (a1, b1) = r^2/l1 .* (x1/l1, y1/l1); # distance of q1' 
+  (a2, b2) = r^2/l2 .* (x2/l2, y2/l2); # distance of q2'
+  q01 = q * r/l1; # image charge of q1 (=q1')
+  q02 = q * r/l2; # image charge of q2 (=q2')
+  return (x,y) -> q * (1/sqrt((x-x1)^2 + (y-y1)^2) - 1/sqrt((x-x2)^2 + (y-y2)^2)) - q01 * (1/sqrt((x-a1)^2 + (y-b1)^2)) + q02 * (1/sqrt((x-a2)^2 + (y-b2)^2))
+end
+
 function main()
-  p1 = Position(1,2);
-  p2 = Position(-1,-2);
-  q = 2.0;
+  p1 = Position(3,4);
+  p2 = Position(-3,-4);
+  q = 10.0;
+  r = 2.0;
 
-  pot = Potential(q,p1,p2);
+  p = Params(q,r);
 
-  x = -5:0.1:5;
-  y = -5:0.1:5;
+  pot = native_dipole(q,p1,p2);
+  pot2 = image_dipole(p,p1,p2);
+
+  x = -10:0.1:10;
+  y = -10:0.1:10;
 
   X = repmat(x', length(y), 1);
   Y = repmat(y, 1, length(x));
-  Z = map(pot,X,Y);
+  Z = map(pot2,X,Y);
   
   surf(x,y,Z)
-  savefig("native_dipole.png")
+  savefig("image_dipole.png")
 end
 
 main()
